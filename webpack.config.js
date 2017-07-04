ac6d227b157f673a91c605e67ec978ddae6b9713
Sergio Rodriguez <oneandprime@gmail.com>
2017-07-04 12:05:35 -0400

    Gotta fix this

--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,6 @@ var webpack = require('webpack')
 
 module.exports = {
   context: __dirname,
-  devtool: debug ? 'inline-sourcemap' : null,
   entry: './js/index.js',
   output: {
     path: __dirname + '/bundle',
